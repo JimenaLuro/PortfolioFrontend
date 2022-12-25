cacd2e951f40ac57fa52c2a6e5baa9d612d8a2ee
JimenaLuro <117375131+JimenaLuro@users.noreply.github.com>
2022-12-25 14:46:12 -0300

    Actualización HTML meta - CSS footer e inicio script en JS

---
+++ script.js
@@ -0,0 +1,10 @@
+// funcion que demuestra el menú responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className==="") {
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+    
+}

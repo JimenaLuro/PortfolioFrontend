b87883ca9d95e8133bbc8ff0083b00653ae1f6fa
JimenaLuro <117375131+JimenaLuro@users.noreply.github.com>
2023-01-01 18:46:24 -0300

    Actualiza script y agrega css responsive

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
-// funcion que demuestra el menú responsive
+// funcion que aplica el estilo a la opción seleccionada en el menú y quita la previamente seleccionada
+
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a')
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+// función para desaparecer el menú una vez seleccionado una opción en el modo responsive
+var x = document.getElementById("nav");
+x.className = "";
+
+}
+
+// funcion que muestra el menú responsive
 function responsiveMenu(){
     var x = document.getElementById("nav");
-    if(x.className==="") {
+    if(x.className === "") {
         x.className = "responsive";
-    }else{
+    } else {
         x.className = "";
     }
     
 }
+
+//detectar srolling para aplicar la animación en barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//función que aplica la animación de la barra de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >=300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+
+}
+
